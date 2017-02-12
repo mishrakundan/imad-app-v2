a663afc6dd3557de63ea35222009a51e4e66c34c
kundan mishra <mishrakundan001@gmail.com>
2017-02-12 16:56:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,15 +94,7 @@ app.get('/:articleName',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
     
 });
-app.get('/article-two',function (req,res){
-    var articleName = req.params.articleName;
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-    }); 
 
-app.get('/article-three',function (req,res){
-    var articleName = req.params.articleName;
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-    });
 
 
 app.get('/ui/style.css', function (req, res) {
