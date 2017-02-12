0fabd740497bf930c2cbd9602dc6c1ffdac1c0b5
kundan mishra <mishrakundan001@gmail.com>
2017-02-12 16:29:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,22 +80,19 @@ return htmlTemplet;
 }
 
 app.get('/', function (req, res) {
-  res.send(ceateTemplate(articles[articleName])); 
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+   
 });
 
 app.get('/:articleName',function (req,res){
     // articleName==article-one
     // articles[articleName]=={}content object for article one
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    var articleName = req.params.articleName;
+    res.send(ceateTemplate(articles[articleName]));
+    
 });
 
-app.get('/article-two',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-three',function (req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
