7b6a8295ad5f8c2824c94ee0621da0d1e7e84af3
kundan mishra <mishrakundan001@gmail.com>
2017-02-12 16:57:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ return htmlTemplet;
 app.get('/', function (req, res) {
     
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-     res.send(ceateTemplate(articles[articleName]));
+    
    
    
 });
@@ -91,7 +91,7 @@ app.get('/:articleName',function (req,res){
     // articleName==article-one
     // articles[articleName]=={}content object for article one   
     var articleName = req.params.articleName;
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(ceateTemplate(articles[articleName]));
     
 });
 
