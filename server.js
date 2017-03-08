e8e8046c8abeed292ee35b1f6957143a520c63e0
kundan mishra <mishrakundan001@gmail.com>
2017-03-08 15:45:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,93 +5,21 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles = {
-'article-one' : {
-    title:  'Article One | kundan mishra',
-    heading: 'Article one',
-    date:'feb 12, 2017',
-    content: ` 
-     <p>
-                This is the content of my first article. This is the content of my first article. This is the content of my first article.
-            </p>
-            <p>
-                 This is the content of my first article. This is the content of my first article. This is the content of my first article.
-            </p>
-            <p>
-                 This is the content of my first article. This is the content of my first article. This is the content of my first article.
-            </p>`  
-},
-'article-two' : {
-    title: 'Article Two | kundan mishra',
-    heading: 'Article two',
-    date:'february 14, 2017',
-    content: ` 
-     <p>
-                This is the content of my second article.
-    </p>`
-        
-},
-'article-three' : {
-    title: 'Article Three | kundan mishra',
-    heading: 'Article three',
-    date:'february 16, 2017',
-    content: `
-     <p>
-       This is the content of my third article.
-    </p>`
-}
-};
-function createTemplate (data) {
-    var title =data.title;
-    var date =data.date;
-    var heading = data.heading;
-    var content=data.content;
-    
-    var htmlTemplate =
-    `<html>
-       <head>
-        <title>
-           ${title}
-      </title>  
-    <meta name="viewport" content="width=device-width,initial-scale=1"/>
-     <link href="/ui/style.css" rel="stylesheet" />
-     
-    </head>    
-    <body>
-        <div class="container">
-         <div>
-              <a href="/">Home</a>
-         </div>
-         <hr/>
-        <h3>
-           ${heading} 
-        </h3>
-        <div>
-            ${date}
-       </div>
-       <div>
-           ${content}
-        </div>
-        </div>
-    </body>
-</html>`
-;
-return htmlTemplate;
-}
-
 app.get('/', function (req, res) {
-         res.sendFile(path.join(__dirname, 'index.html'));
-     
-    });
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
-app.get('/:articleName',function (req,res){
-    // articleName==article-one
-    // articles[articleName]== {} content object for article one   
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/article-one',function (req,res){
+    res.send('Article one requested and will be served here')
+});
 
-});    
+app.get('/article-two',function (req,res){
+    res.send('Article two requested and will be served here')
+});
 
+app.get('/article-three',function (req,res){
+    res.send('Article three requested and will be served here')
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
