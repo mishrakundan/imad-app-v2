3d0380d306cfc1122c92e8f11f012b50d88ebee8
kundan mishra <mishrakundan001@gmail.com>
2017-03-08 18:28:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req,res){
-    res.send(createTemplate(article-One));
+    res.send(createTemplate(article-one));
 });
 
 app.get('/article-two',function (req,res){
