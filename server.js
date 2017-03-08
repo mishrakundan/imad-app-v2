fb5e21a6548f576992d91f8951c809d306ad8ca3
kundan mishra <mishrakundan001@gmail.com>
2017-03-08 19:02:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,21 +5,55 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne={
-    title:'Article One | kundan mishra',
-    heading:'Article one',
-    date: 'feb 12,2017',
-    content:`
-          <p>
-                This is the content of my first article. This is the content of my first article. This is the content of my first article.
-         </p>
-         <p>
-                 This is the content of my first article. This is the content of my first article. This is the content of my first article.
-         </p>
-         <p>
-                 This is the content of my first article. This is the content of my first article. This is the content of my first article.
-         </p>`
-};
+
+var articles={
+      'article-one':{
+        title:'Article One | kundan mishra',
+        heading:'Article one',
+        date: 'feb 12,2017',
+        content:`
+              <p>
+                    This is the content of my first article. This is the content of my first article. This is the content of my first article.
+             </p>
+             <p>
+                     This is the content of my first article. This is the content of my first article. This is the content of my first article.
+             </p>
+             <p>
+                     This is the content of my first article. This is the content of my first article. This is the content of my first article.
+             </p>`
+    },
+      'article-two':{
+        title:'Article Two | kundan mishra',
+        heading:'Article two',
+        date: 'feb 12,2017',
+        content:`
+              <p>
+                    This is the content of my second article. This is the content of my second article. This is the content of my second article.
+             </p>
+             <p>
+                     This is the content of my second article. This is the content of my second article. This is the content of my second article.
+             </p>
+             <p>
+                     This is the content of my second article. This is the content of my second article. This is the content of my second article.
+             </p>`
+    },
+      'article-three': {
+        title:'Article Three | kundan mishra',
+        heading:'Article three',
+        date: 'feb 12,2017',
+        content:`
+              <p>
+                    This is the content of my thirs article. This is the content of my third article. This is the content of my third article.
+             </p>
+             <p>
+                     This is the content of my third article. This is the content of my third article. This is the content of my third article.
+             </p>
+             <p>
+                     This is the content of my third article. This is the content of my third article. This is the content of my third article.
+             </p>`
+          
+      }
+};      
 
 function createTemplate(data){
     var title = data.title;
@@ -61,16 +95,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function (req,res){
-    res.send(createTemplate(articleOne));
-});
-
-app.get('/article-two',function (req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three',function (req,res){
-      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName',function (req,res){
+    //articleName==article-one
+    //articles[articleName]=={} content object of article one
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
