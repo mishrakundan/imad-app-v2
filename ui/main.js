f6d184e75f59d24e1f0e6d1d792869b2d8f85283
kundan mishra <mishrakundan001@gmail.com>
2017-03-10 16:44:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 // change the text of the main-text div
-var img=document.getElementById('main-text');
+var element=document.getElementById('main-text');
 element.innerHTML =' Hi! I am Kundan Mishra,and this is my first webapp.';
 
 //Move the image
