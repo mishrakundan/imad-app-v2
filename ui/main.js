f9571c0f61db004603acb9431399a4a35ca7df2b
kundan mishra <mishrakundan001@gmail.com>
2017-03-10 15:40:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+//Move the image
+var img=document.getElementById('madi');
+var marginleft =0;
+function MoveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.oneclick = function (){
+    var intervel = set1interval(moveRight, 100);
+    
+};
