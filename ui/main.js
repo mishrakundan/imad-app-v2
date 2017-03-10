1015fe265495a3814ef13058e858a0e7dbb011ae
kundan mishra <mishrakundan001@gmail.com>
2017-03-10 18:37:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,5 @@
 console.log('Loaded!');
+// change the text of the main-text div
 
+var element=document.getElementById('main-text');
+element.innerHTML =' Hi! I am Kundan Mishra,and this is my first webapp.';
