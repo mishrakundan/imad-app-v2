128268c853fdc98c0ea1db3cdd0954f6c1f39ad6
kundan mishra <mishrakundan001@gmail.com>
2017-03-10 19:02:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,13 @@ var element=document.getElementById('main-text');
 element.innerHTML =' Hi! I am Kundan Mishra,and this is my first webapp.';
 
 //Move the image
-var img = document.getElementById('madi');
-    img.style.marginLeft = '100px';
+    var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight (){
+    marginLeft = marginLeft + 5;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.oneclick = function (){
-    };
+    var interval = setInterval(moveRight, 50);
+    
+};
