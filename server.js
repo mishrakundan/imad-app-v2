cf659e160e412bafb93e7032649c2ccd68cbd4a3
kundan mishra <mishrakundan001@gmail.com>
2017-03-18 11:35:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+  host: 'db.imad.hasura-app.io',
+  user: 'mishrakundan',
+  database: 'mishrakundan',
+  port:'5432',
+  password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -96,7 +104,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var pool = new Pool(config);
+app.get('/test-db',function(req,res) {
+    // make a select request
+    //return a response with the result
+    pool.query('SELECT * FROM test',function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        } else{
+            res.send(JSON.stringify(result));
+        }
+    });
+});
 
 app.get('/:articleName',function (req,res){
     //articleName==article-one
